22bb27e5b9f169259fd68d407af1f8979cf5d692
alallala <64277365+alallala@users.noreply.github.com>
2020-05-14 23:08:54 +0200

    Update alexnet.py

--- alexnet.py
+++ alexnet.py
@@ -70,6 +70,7 @@ def alexnet(pretrained=False, progress=True, **kwargs):
         state_dict = load_state_dict_from_url(model_urls['alexnet'],
                                               progress=progress)
         model.load_state_dict(state_dict,strict=False)
-        #model.domain_classifier.load_state_dict(model.classifier.state_dict())
-        #model.domain_classifier[6] = nn.Linear(4096,2)
+        model.domain_classifier[6] == nn.Linear(4096,num_classes)
+        model.domain_classifier.load_state_dict(model.classifier.state_dict())
+        model.domain_classifier[6] = nn.Linear(4096,2)
     return model
