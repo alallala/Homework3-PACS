365034f08f04241c9f730ff11acbbba33071d4cc
alallala <64277365+alallala@users.noreply.github.com>
2020-05-15 18:12:52 +0200

    Update my_alexnet.py

--- my_alexnet.py
+++ my_alexnet.py
@@ -74,7 +74,7 @@ class AlexNet(nn.Module):
         if alpha is not None:
             # gradient reversal layer (backward gradients will be reversed)
             reverse_feature = ReverseLayerF.apply(features, alpha)
-            discriminator_output = self.dann_classifier(reverse_feature)
+            discriminator_output = self.domain_classifier(reverse_feature)
         
             return discriminator_output
         # If we don't pass alpha, we assume we are training with supervision
