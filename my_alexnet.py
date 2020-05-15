4dbb2cf3c332291cbfdab1052107789197c493ef
alallala <64277365+alallala@users.noreply.github.com>
2020-05-15 16:57:33 +0200

    Update my_alexnet.py

--- my_alexnet.py
+++ my_alexnet.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 from torch.hub import load_state_dict_from_url
 
 
-#__all__ = ['AlexNet', 'alexnet']
+__all__ = ['AlexNet', 'alexnet']
 
 
 model_urls = {
@@ -64,7 +64,7 @@ def alexnet(pretrained=False, progress=True, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    model = AlexNet(**kwargs)
+    model = AlexNet(**kwargs,1000)
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls['alexnet'],
                                               progress=progress)
