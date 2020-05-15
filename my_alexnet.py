d9e1b0a4461a3966c8b9c7457566254d8c103aa2
alallala <64277365+alallala@users.noreply.github.com>
2020-05-15 17:04:53 +0200

    Update my_alexnet.py

--- my_alexnet.py
+++ my_alexnet.py
@@ -67,7 +67,7 @@ def alexnet(pretrained=False, progress=True, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    model = AlexNet(**kwargs,num_classes)
+    model = AlexNet(**kwargs)
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls['alexnet'],
                                               progress=progress)
